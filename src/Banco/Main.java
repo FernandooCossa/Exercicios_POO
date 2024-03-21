8486e70c51415f31b61a47c0d9f7a779324c24cf
Fernando de Souza Cossa <fernando_s_de-cossa@estudante.sesisenai.org.br>
2024-03-20 21:43:45 -0300

    Add Exercicios

--- src/Banco/Main.java
+++ src/Banco/Main.java
@@ -6,11 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco();
         Scanner scanner = new Scanner(System.in);
+        //titular
         System.out.println("Titular: ");
         banco.titular = scanner.next();
+        //depositar
         System.out.println("Informe o valor do deposito : ");
         double valor = scanner.nextDouble();
         banco.depositarDinheiro(valor);
+        //sacar
         System.out.println("Informe o valor do saque : ");
         valor = scanner.nextDouble();
         banco.sacarDinheiro(valor);
